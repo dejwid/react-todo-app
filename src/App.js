105f99fe1100e9211a64961cba54beb58096fab5
dejwid <dawid.paszko@gmail.com>
2022-11-05 21:20:37 +0100

    Adding a localStorage defaults

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     const tasks = JSON.parse(localStorage.getItem('tasks'));
-    setTasks(tasks);
+    setTasks(tasks || []);
   }, []);
 
   function addTask(name) {
